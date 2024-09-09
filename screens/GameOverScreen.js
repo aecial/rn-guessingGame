2cd59252b16e1e57d868c56a080fef00ad1fa94d
Teddy Pascual <teddylayson2000@gmail.com>
2024-09-09 22:00:37 +0800

    add dynamic width and height using max

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -51,6 +51,8 @@ const styles = StyleSheet.create({
   imgContainer: {
     width: 250,
     height: 250,
+    maxWidth: "75%",
+    maxHeight: "40%",
     borderRadius: 125,
     borderWidth: 3,
     borderColor: Colors.primary,
